0ee1636ea37d8b78a525da389ca141f281890a02
GitHub Action <action@github.com>
2022-01-19 03:47:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/dingzhi86/demo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dingzhi86/demo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dingzhi86.demo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dingzhi86.demo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/dingzhi86/demo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dingzhi86/demo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dingzhi86.demo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dingzhi86.demo.MyBundle
 
 class MyProjectService(project: Project) {
 
